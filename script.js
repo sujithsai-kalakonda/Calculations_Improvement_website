9dacba5caf07cec5cb2f4f65b7487dc2cb32ee64
sujithsai-kalakonda <108214819+sujithsai-kalakonda@users.noreply.github.com>
2023-10-01 17:19:12 +0530

    Update script.js by formating

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ let numDigits;
 let numQuestions;
 let currentQuestion;
 
-let appendedAnswer = ''; // This stores the value of keyn entered by the user.
+let appendedAnswer = '';
 
 function generateRandomNumber(digits) {
-    const min = Math.pow(10, digits - 1);
-    const max = Math.pow(10, digits) - 1;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+	const min = Math.pow(10, digits - 1);
+	const max = Math.pow(10, digits) - 1;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function generateQuestion() {
-    let questionText = '';
-    let answer = 0;
-    for (let i = 0; i < numQuestions; i++) {
-        const num = generateRandomNumber(numDigits);
-        questionText += `${num} ${operator} `;
-        if (i === 0) {
-            answer = num;
-        } else {
-            answer = eval(`${answer} ${operator} ${num}`);
-        }
-    }
-    questionText = questionText.slice(0, -2); // Remove trailing operator
-    return { questionText, answer };
+	let questionText = '';
+	let answer = 0;
+	for (let i = 0; i < numQuestions; i++) {
+		const num = generateRandomNumber(numDigits);
+		questionText += `${num} ${operator} `;
+		if (i === 0) {
+			answer = num;
+		} else {
+			answer = eval(`${answer} ${operator} ${num}`);
+		}
+	}
+	questionText = questionText.slice(0, -2); // Remove trailing operator
+	return { questionText, answer };
 }
 
 function displayCorrectAnswer(answer) {
@@ -34,20 +34,20 @@ function displayCorrectAnswer(answer) {
 
 	setTimeout(() => {
 		answerDisplay.style.opacity = '0';
-}, 900);
+	}, 900);
 }
 
 function checkAnswer() {
 	const userAnswer = parseFloat(appendedAnswer); // Use the appended answer
 
 	if (userAnswer === currentQuestion.answer) {
-			showFeedback('check-mark.png', 'correct'); // Use the tick image
-			// Record correct answer and time taken here
-			// Update statistics
+		showFeedback('check-mark.png', 'correct'); // Use the tick image
+		// Record correct answer and time taken here
+		// Update statistics
 	} else {
-			showFeedback('cancel.png', 'incorrect'); // Use the cross image
-			const formattedAnswer = currentQuestion.answer.toFixed(2); // Format the correct answer
-			displayCorrectAnswer(formattedAnswer);
+		showFeedback('cancel.png', 'incorrect'); // Use the cross image
+		const formattedAnswer = currentQuestion.answer.toFixed(2); // Format the correct answer
+		displayCorrectAnswer(formattedAnswer);
 	}
 }
 
@@ -59,60 +59,60 @@ function showFeedback(imageSrc, className) {
 
 	// Hide the feedback after 2 seconds
 	setTimeout(() => {
-			feedbackImage.style.opacity = '0';
+		feedbackImage.style.opacity = '0';
 	}, 900);
 }
 
 
 function generateNextQuestion() {
-    // Clear the answer display
-    const answerDisplay = document.getElementById('answer-display');
-    answerDisplay.innerHTML = '';
-    answerDisplay.style.opacity = '0';
+	// Clear the answer display
+	const answerDisplay = document.getElementById('answer-display');
+	answerDisplay.innerHTML = '';
+	answerDisplay.style.opacity = '0';
 
-    // Clear the feedback
-    const feedback = document.getElementById('feedback');
-    feedback.innerHTML = '';
-    feedback.style.opacity = '0';
+	// Clear the feedback
+	const feedback = document.getElementById('feedback');
+	feedback.innerHTML = '';
+	feedback.style.opacity = '0';
 
-    // Hide the feedback images
-    const correctImage = document.getElementById('correct');
-    const incorrectImage = document.getElementById('incorrect');
-    correctImage.style.opacity = '0';
-    incorrectImage.style.opacity = '0';
+	// Hide the feedback images
+	const correctImage = document.getElementById('correct');
+	const incorrectImage = document.getElementById('incorrect');
+	correctImage.style.opacity = '0';
+	incorrectImage.style.opacity = '0';
 
-    currentQuestion = generateQuestion();
-    document.getElementById('question').innerHTML = currentQuestion.questionText;
-    document.getElementById('userAnswer').value = '';
-    document.getElementById('result').innerHTML = '';
+	currentQuestion = generateQuestion();
+	document.getElementById('question').innerHTML = currentQuestion.questionText;
+	document.getElementById('userAnswer').value = '';
+	document.getElementById('result').innerHTML = '';
 }
 
 
 function exitChallenge() {
-    document.getElementById('config').style.display = 'block';
-    document.getElementById('challenge').style.display = 'none';
+	document.getElementById('config').style.display = 'block';
+	document.getElementById('challenge').style.display = 'none';
 }
 
 function startChallenge() {
-    numDigits = parseInt(document.getElementById('numDigits').value) || 1; // making 1 as default value
-    numQuestions = parseInt(document.getElementById('numQuestions').value) || 2;
-    currentQuestion = generateQuestion();
-    document.getElementById('question').innerHTML = currentQuestion.questionText;
+	numDigits = parseInt(document.getElementById('numDigits').value) || 1; // making 1 as default value
+	numQuestions = parseInt(document.getElementById('numQuestions').value) || 2;
+	currentQuestion = generateQuestion();
+	document.getElementById('question').innerHTML = currentQuestion.questionText;
 
-    appendedAnswer = '';
-    
-    document.getElementById('config').style.display = 'none';
-    document.getElementById('challenge').style.display = 'block';
+	appendedAnswer = '';
 
-    // Show the keypad and the input field
-    document.getElementById('keypad').style.display = 'block';
-    document.getElementById('userAnswer').style.display = 'block';
+	document.getElementById('config').style.display = 'none';
+	document.getElementById('challenge').style.display = 'block';
+
+	// Show the keypad and hide the input field
+	document.getElementById('keypad').style.display = 'block';
+	document.getElementById('userAnswer').style.display = 'block';
 }
 
 function selectOperator(selectedOperator) {
 	operator = selectedOperator;
 	document.getElementById('selectedOperator').innerHTML = selectedOperator;
-	
+
 	// Highlight the selected operator button
 	const operatorButtons = document.querySelectorAll('#operatorButtons button');
 	operatorButtons.forEach(button => button.classList.remove('selected'));
@@ -125,4 +125,3 @@ function appendToAnswer(character) {
 	var inputField = document.getElementById('userAnswer');
 	inputField.value = appendedAnswer;
 }
-
